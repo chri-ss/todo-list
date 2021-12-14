65e3c90adee519c984d0b8446633f994b8a52696
Chris Cresswell <christorper@gmail.com>
2021-12-13 21:46:29 -0800

    Add todo and project factories

--- bundle.js
+++ bundle.js
@@ -1,1 +1,1 @@
-console.log("hi");
+(()=>{"use strict";const o={title:e="stuff",description:"do some stuff",dueDate:"12/12/21",priority:"high",getTitle:()=>console.log(e)},t={todo:o};var e;console.log(o),console.log(t)})();
