bf45df650a1d181384a1f272d8e15c5cc0cebad5
Chris Cresswell <christorper@gmail.com>
2021-12-13 21:54:51 -0800

    Make project factory accept multiple arguments

--- bundle.js
+++ bundle.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";const o={title:e="stuff",description:"do some stuff",dueDate:"12/12/21",priority:"high",getTitle:()=>console.log(e)},t={todo:o};var e;console.log(o),console.log(t)})();
+(()=>{"use strict";const o=(o,e,t,s)=>({title:o,description:e,dueDate:t,priority:s,getTitle:()=>console.log(o)}),e=o("stuff","do some stuff","12/12/21","high"),t=((...o)=>({...o}))(e,o("some other stuff","do something better","12/13/21","higher"));console.log(e),console.log(t)})();
